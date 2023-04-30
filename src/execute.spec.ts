690d941674ff2b55c3f38b97e3e8e1350ee87b01
Phoscur <phoscur@pheelgood.net>
2023-04-30 19:10:00 +0200

    Optimize trim GraphQL test query

--- src/execute.spec.ts
+++ src/execute.spec.ts
@@ -16,90 +16,9 @@ describe("GraphQL Mesh", () => {
       }
 
       fragment LayoutData on Layout {
-        privacy
-        geometry
-        hashId
-        parent {
-          hashId
-          __typename
-        }
-        tags {
-          id
-          hashId
-          name
-          __typename
-        }
         title
-        user {
-          annotation
-          annotationPublic
-          name
-          hashId
-          pictureUrl
-          __typename
-        }
-        revision {
-          ...RevisionData
-          __typename
-        }
         lastRevisionCompiled
         isLatestRevision
-        __typename
-      }
-
-      fragment RevisionData on Revision {
-        createdAt
-        hashId
-        hexUrl
-        model
-        title
-        config
-        swatch
-        zipUrl
-        qmkVersion
-        qmkUptodate
-        md5
-        combos {
-          keyIndices
-          layerIdx
-          name
-          trigger
-          __typename
-        }
-        tour {
-          ...TourData
-          __typename
-        }
-        layers {
-          builtIn
-          hashId
-          keys
-          position
-          title
-          color
-          prevHashId
-          __typename
-        }
-        __typename
-      }
-
-      fragment TourData on Tour {
-        hashId
-        url
-        steps: tourSteps {
-          hashId
-          intro
-          outro
-          position
-          content
-          keyIndex
-          layer {
-            hashId
-            position
-            __typename
-          }
-          __typename
-        }
         __typename
       }
     `;
