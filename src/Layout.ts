1889d673e97655c8a88e61f62d031a08c04039ec
Phoscur <phoscur@pheelgood.net>
2023-04-30 19:09:41 +0200

    Add prompts for layer copy selection

--- src/Layout.ts
+++ src/Layout.ts
@@ -1,14 +1,42 @@
+import type { Layer as LayerBaseType } from "../.mesh";
 import { getMeshSDK, Sdk, MyLayout } from "../.mesh";
+import storage from "node-persist";
 
+export type KeyAction = {
+  code: string;
+  color: string | null;
+  layer: null; // TODO?
+  macro: null; // TODO
+  modifier: null;
+  modifiers: null;
+  description: string | null;
+}
 
-export default class OryxLayout {
-  constructor(
-    protected sdk: Sdk = getMeshSDK(),
-  ) {}
+export type LayerKey = {
+  about: string | null;
+  aboutPosition: string | null;
+  customLabel: string | null;
+  doubleTap: KeyAction | null;
+  emoji: string | null;
+  glowColor: string | null;
+  hold: KeyAction | null;
+  icon: string | null;
+  tap: KeyAction | null;
+  tapHold: KeyAction | null;
+};
 
-  async createLayer(revisionHashId: string, position: number, title = "Layer") {
-    const newKeys = [];
-    for (let i = 0; i < 72; i++) {
+/**
+ * LayerBaseType override for better keys attribute typing
+ */
+export interface Layer extends LayerBaseType {
+  keys: LayerKey[];
+}
+
+export class OryxLayout {
+  constructor(protected sdk: Sdk = getMeshSDK()) {}
+
+  async createLayer(revisionHashId: string, position: number, newKeys: LayerKey[] = [], title = "Layer") {
+    while (newKeys.length < 72) { // TODO? use layout geometry attribute to derive keycount
       newKeys.push({
         about: null,
         aboutPosition: null,
@@ -28,52 +56,69 @@ export default class OryxLayout {
       position,
       title,
     });
-    const { hashId } = layer.createLayer;
-    return new OryxLayoutLayer(hashId, this.sdk);
+    return layer.createLayer;
   }
 }
 
-export class LayoutSdk extends OryxLayout {
-  constructor(
-    public data: MyLayout,
-    sdk: Sdk = getMeshSDK(),
-  ) {
+export default class MyLayoutSdk extends OryxLayout {
+  public layers: LayerSdk[];
+  constructor(public data: MyLayout, sdk: Sdk = getMeshSDK()) {
     super(sdk);
+    this.layers = this.data.revisions[0].layers.map(l => new LayerSdk(l as Layer, sdk));
   }
 
   static async getMine(sdk: Sdk = getMeshSDK()) {
     const { myLayouts } = await sdk.myLayouts_query({});
-    return myLayouts.map(l => new LayoutSdk(l, sdk));
+    await storage.setItem("myLayouts", myLayouts);
+    return myLayouts.map((l) => new MyLayoutSdk(l, sdk));
   }
 
-  async getNewLayer(title: string = 'New', position?: number) {
-    const layers = this.data.revisions[0].layers
+  static async fromStorage(sdk: Sdk = getMeshSDK()): Promise<MyLayoutSdk[]> {
+    const myLayouts = await storage.getItem("myLayouts");
+    return myLayouts.map((l) => new MyLayoutSdk(l, sdk));
+  }
+
+  // TODO? split out getExistingLayer
+  async getNewLayer(title: string = "New", position?: number) {
+    const layers = this.data.revisions[0].layers as Layer[];
     if (layers[position]?.title === title) {
-      console.log('Existing layer found:', title);
-      return new OryxLayoutLayer(layers[position].hashId, this.sdk);
+      console.log("Existing layer found:", title);
+      return new LayerSdk(layers[position], this.sdk);
     } else {
-      console.log("Layer", title, "does not exist yet", layers[position]);
+      console.log("Layer", title, "does not exist yet", position, layers[position]);
     }
     const revisionHashId = this.data.revisions[0].hashId;
     const layerCount = this.data.revisions[0].layers.length;
-    return this.createLayer(revisionHashId, position ?? layerCount, title);
+    const { hashId } = await this.createLayer(revisionHashId, position ?? layerCount, [], title);
+    console.log("Created new layer:", hashId);
+    const { myLayouts } = await this.sdk.myLayouts_query();
+    await storage.setItem("myLayouts", myLayouts);
+    this.data = myLayouts.find((l) => l.hashId === this.data.hashId);
+    const layer = new LayerSdk(this.data.revisions[0].layers.find((l) => l.hashId === hashId) as Layer, this.sdk);
+    if (!this.data || !layer) {
+      console.log("Layer", layer);
+      console.log("Layout", this.data);
+      throw new Error(`Failed to create new Layer "${title}" [${hashId}]`);
+    }
+    return layer;
   }
 
   toString() {
     const { title, hashId, revisions } = this.data;
-    return `${title} [${hashId}]: ${revisions[0].layers.length} layers [${revisions[0].hashId}]`;
+    return `${title} [${hashId}]: ${revisions[0].layers.length} layers [${revisions[0].hashId}] (${this.layersList.join(", ")})`;
+  }
+
+  get layersList() {
+    return this.layers.map(l => l.toString());
   }
 }
 
-export class OryxLayoutLayer {
-  constructor(
-    public readonly hashId: string,
-    private sdk: Sdk = getMeshSDK(),
-  ) {}
+export class LayerSdk {
+  constructor(public readonly data: Layer, private sdk: Sdk = getMeshSDK()) {}
 
   setKey(code: string, position: number, customLabel = null) {
     return this.sdk.updateKey_mutation({
-      hashId: this.hashId,
+      hashId: this.data.hashId,
       keyData: {
         about: null,
         aboutPosition: null,
@@ -97,5 +142,25 @@ export class OryxLayoutLayer {
       },
       position,
     });
+  }
+
+  toString() {
+    return `${this.data.title}`;// [${this.data.hashId}]`;// ${this.keysAsString}`;
+  }
+
+  get keysAsString() {
+    return `[${this.data.hashId}]: ${this.data.keys.map(k => {
+      if (k.customLabel) {
+        return k.customLabel;
+      }
+      if (!k.tap) {
+        return `•`
+      }
+      const code = k.tap.code.replace("KC_", "")
+      if (code === "TRANSPARENT") {
+        return " ";
+      }
+      return code;
+    }).join(" ")}`;
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,78 @@
 import { getMeshSDK } from "../.mesh";
-import OryxLayout, { LayoutSdk, OryxLayoutLayer } from "./Layout";
+import MyLayoutSdk from "./Layout";
+import prompts from "prompts";
+import storage from "node-persist";
+prompts.override(require('yargs').argv);
 
-async function test() {
-  const sdk = getMeshSDK();
-  const myLayouts = await LayoutSdk.getMine();
-  console.log(`Found myLayouts(${myLayouts.length}):`); 
-  for (const l of myLayouts) {
-    console.log(`- ${l}`);
+if (!process.env.TOKEN) {
+  console.log("Please set a authentication TOKEN in the .env-file first");
+  process.exit(1);
+}
+
+
+async function selectLayouts(myLayouts: MyLayoutSdk[]): Promise<{
+  layout: MyLayoutSdk,
+  source: MyLayoutSdk,
+}> {
+  const layout = myLayouts.find(l => l.data.hashId === process.env.PRESELECT_LAYOUT);
+  const source = myLayouts.find(l => l.data.hashId === process.env.PRESELECT_SOURCE_LAYOUT);
+
+  if (layout || source) {
+    prompts.override({ layout, source });
   }
+
+  return prompts([
+    {
+      type: 'select',
+      name: 'layout',
+      message: `Which layout are we working on?`,
+      choices: myLayouts.map(l => ({
+        title: `${l.data.title} [${l.data.hashId}]`,
+        value: l,
+        description: l.layers.map(layer => `${layer} ${layer.keysAsString}`).join("\n"),
+      })),
+    },
+    {
+      type: 'select',
+      name: 'source',
+      message: 'Pick source layout',
+      choices: myLayouts.map(l => ({
+        title: `${l.data.title} [${l.data.hashId}]`,
+        value: l,
+        description: l.layers.map(layer => `${layer} ${layer.keysAsString}`).join("\n"),
+      }))
+    }
+  ]);
+}
+
+(async function main() {
+  await storage.init({ dir: "storage" });
+  const values = await storage.values();
+  console.log(values.length > 0 ? `Found & using cached layouts!` : `Layout Cache is empty.`);
+  const sdk = getMeshSDK();
+  // TODO refresh storage on demand & timeout?
+  const myLayouts = await (values.length > 0 ? MyLayoutSdk.fromStorage(sdk) : MyLayoutSdk.getMine(sdk));
+  console.log(`Found myLayouts (${myLayouts.length}):`); 
+  for (const l of myLayouts) {
+    console.log(`- ${l}\n`);
+  }
+  const { layout, source } = await selectLayouts(myLayouts);
+
+  const response = await prompts([
+    {
+      type: 'select',
+      name: 'layer',
+      message: 'Pick source layer',
+      choices: source.layers.map(layer => ({
+        title: layer.data.title,
+        value: layer,
+        description: `${layer.keysAsString}`
+      }))
+    }
+  ]);
+
+  console.log(`Selected Layer: ${response.layer}`);
+
   //const revisionHashId = myLayouts[0].revisions[0].hashId, layerCount = myLayouts[0].revisions[0].layers.length;
   //console.log(myLayouts[0].title, "latest revision hash", revisionHashId);
   // console.log("layer 5", myLayouts[0].revisions[0].layers[5]);
@@ -18,15 +83,14 @@ async function test() {
     title: "Layer",
   };*/
   //const layout = new OryxLayout(sdk);
-  const mine = myLayouts[0];
-  // TODO check if it has the correct hash
+  //const mine = myLayouts[0];
+  // TODO check if it has the correct hash & if lastRevisionCompiled / isLatestRevision
   //const l = await layout.createLayer(revisionHashId, layerCount, 'Test');
-  const position = 5;
-  const l = await mine.getNewLayer('Layer', position);
+  //const position = 6;
+  //const l = await mine.getNewLayer('UwU', position);
   //console.log("Created Layer:", layerCount, l.hashId, );
-  const customLabel = "A", code = "KC_A";
-    //hashId = mine.data.revisions[0].layers[position].hashId;
+  //const customLabel = "A", code = "KC_A";
+  //hashId = mine.data.revisions[0].layers[position].hashId;
   //const layer = new OryxLayoutLayer(hashId, sdk);
-  console.log(customLabel, code, await l.setKey(code, 26, "AAA"));
-}
-test();
+  //console.log(customLabel, code, await l.setKey(code, 26, "AAA"));
+})();
